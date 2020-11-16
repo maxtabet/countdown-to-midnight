e271ea9bd13d0ef4ab43280d316b805b486e691a
Maximilian Tabet <maxtabet@gmail.com>
2020-11-16 16:27:23 -0500

    commit tree 0373ed45537cc61856886900d0398a872930ab79

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+seconds_to_midnight = 10
 
-def countdown
+def countdown_with_sleep(countdown)
+    sleep(5)
+
+end
+
+def countdown(seconds_to_midnight)
+    while seconds_to_midnight > 0
+        puts "#{seconds_to_midnight} SECOND(S)!"
+        seconds_to_midnight -= 1
+    end
+    if seconds_to_midnight == 0
+        return "HAPPY NEW YEAR!"
+    end
+
 end
